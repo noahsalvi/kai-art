82ef8a7c988af3ab9d454e65fe7749729f40128c
Noah Salvi <noah.salvi@ti8m.ch>
2021-05-01 16:39:49 +0200

    update gitignore

--- .svelte/output/client/_app/chunks/pageFade-a3c22aee.js
+++
@@ -1,1 +0,0 @@
-import{R as a}from"./vendor-499072eb.js";const s=(s,o)=>({duration:100,delay:100,easing:a,css:a=>`opacity: ${a}`}),o=(s,o)=>({duration:100,delay:0,easing:a,css:a=>`opacity: ${a}`});export{s as a,o as f};
