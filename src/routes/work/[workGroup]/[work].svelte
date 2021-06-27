ce8f2815c4ad0c5e0dbfed4a203ea8fbf4e223db
Noah Salvi <noah.salvi@ti8m.ch>
2021-06-27 21:25:44 +0200

    fix layout on [work] page

--- src/routes/work/[workGroup]/[work].svelte
+++ src/routes/work/[workGroup]/[work].svelte
@@ -31,8 +31,10 @@
   });
 </script>
 
-<div class="relative h-full max-w-10xl mx-auto">
-  <div class="h-10 max-w-7xl mx-auto px-5 sm:px-20 z-10 text-gray font-sans">
+<div class="relative h-full max-w-10xl mx-auto flex flex-col">
+  <div
+    class="h-10 max-w-7xl mx-auto px-5 sm:(px-20 w-full) z-10 text-gray font-sans"
+  >
     <a href="/work">Werke</a> /
     <a href=".">{work.category}</a> /
     <span>{work.name}</span>
@@ -42,9 +44,9 @@
     class="absolute left-0 top-30 bg-primary h-50 w-6/10 md:w-2/5 rounded-xl"
   />
 
-  <div class="h-full -mt-10 font-sans">
+  <div class="flex-grow mt-30 font-sans">
     <main
-      class="relative min-h-3xl flex -md:flex-col justify-between mt-50 bg-white mx-6 p-10 rounded-xl"
+      class="relative min-h-2xl flex -md:flex-col justify-between bg-white mx-6 p-10 rounded-xl"
     >
       <div class="mx-auto max-w-[75ch] md:mx-0">
         <Sotion {blocks} />
