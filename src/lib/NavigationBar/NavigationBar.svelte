9e46927a13fcc3f817014014658233834fb0759e
Noah Salvi <noah.salvi@ti8m.ch>
2021-06-28 16:12:24 +0200

    center navigation vertically on small screens

--- src/lib/NavigationBar/NavigationBar.svelte
+++ src/lib/NavigationBar/NavigationBar.svelte
@@ -10,13 +10,16 @@
 <nav
   class="h-24 p-6 px-5 space-x-4 sm:px-20 flex justify-between items-center w-full max-w-7xl mx-auto"
 >
-  <a href="/" class="flex-basis-full flex items-end space-x-4 h-full">
+  <a
+    href="/"
+    class="flex-basis-full flex items-center space-x-4 h-full sm:items-end"
+  >
     <!-- TODO improve log svg and add white border to it directly -->
 
     <img
       src="/logo.svg"
       alt="Logo"
-      class="w-16 max-w-full  rounded-lg  transition-all border sm:(w-auto h-full) hover:text-opacity-25 {logoBorderColor}"
+      class="w-16 max-w-full rounded-lg transition-all border sm:(w-auto h-full) hover:text-opacity-25 {logoBorderColor}"
     />
     <h1
       class="font-thin hidden text-2xl hidden whitespace-nowrap md:block lg:(text-3xl) {whiteTitle}"
@@ -26,7 +29,7 @@
   </a>
 
   <ul
-    class="flex space-x-5 items-end h-full text-white w-full justify-end text-md sm:(text-lg)"
+    class="flex space-x-5 items-center h-full text-white w-full justify-end text-md sm:(text-lg items-end)"
   >
     <li><NavigationLink path="/work">Werke</NavigationLink></li>
     <li><NavigationLink path="/portrait">Portrait</NavigationLink></li>
