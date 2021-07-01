1307aff7da85dbc54590f432080aeec96561689d
Noah Salvi <noah.salvi@ti8m.ch>
2021-07-01 21:30:48 +0200

    finish logic for desktop image viewer

--- src/lib/ImageViewer/ImageViewer.svelte
+++ src/lib/ImageViewer/ImageViewer.svelte
@@ -14,14 +14,29 @@
   setContext(imagesKey, images);
 
   let imageIndex = 0;
+  let bottomHeight: number;
 
   $: src = $images[imageIndex];
+  $: hasPrevious = imageIndex;
+  $: hasNext = imageIndex + 1 < $images.length;
 
-  const previousImage = () => (imageIndex ? imageIndex-- : imageIndex);
-  const nextImage = () =>
-    imageIndex + 1 < $images.length ? imageIndex++ : imageIndex;
+  const previousImage = () => (hasPrevious ? imageIndex-- : imageIndex);
+  const nextImage = () => (hasNext ? imageIndex++ : imageIndex);
 
-  const close = () => ($activated = false);
+  const close = () => {
+    $activated = false;
+    imageIndex = 0;
+  };
+
+  const handleKeyNavigation = (e: KeyboardEvent) => {
+    if (e.key === "ArrowRight") {
+      nextImage();
+    } else if (e.key === "ArrowLeft") {
+      previousImage();
+    } else if (e.key === "Escape") {
+      close();
+    }
+  };
 </script>
 
 <slot />
@@ -31,15 +46,17 @@
     on:click={close}
     class="z-100 fixed left-0 top-0 h-screen w-screen bg-black bg-opacity-85"
   >
-    <div class="w-full h-full flex">
-      <div class="w-60 h-full flex justify-center items-center">
+    <div style="height: calc(100% - {bottomHeight}px)" class="flex">
+      <div class="w-60 h-full flex-shrink-0 flex justify-center items-center">
         <!-- Left Button -->
-        <div on:click|stopPropagation={previousImage} class="switch-button">
-          &lt;
-        </div>
+        {#if hasPrevious}
+          <div on:click|stopPropagation={previousImage} class="switch-button">
+            &lt;
+          </div>
+        {/if}
       </div>
       <!-- Image Container -->
-      <div class="flex-grow h-full flex justify-center">
+      <div class="flex-grow h-full flex justify-center sm:pt-20">
         <img
           on:click|stopPropagation
           class="h-full max-w-full object-contain"
@@ -47,18 +64,39 @@
           alt={{}.toString()}
         />
       </div>
-      <div class="w-60 h-full flex justify-center items-center">
+      <div class="w-60 h-full flex-shrink-0 flex justify-center items-center">
         <!-- Right Button -->
-        <div on:click|stopPropagation={nextImage} class="switch-button">
-          &gt;
-        </div>
+        {#if hasNext}
+          <div on:click|stopPropagation={nextImage} class="switch-button">
+            &gt;
+          </div>
+        {/if}
+      </div>
+    </div>
+    <div
+      bind:clientHeight={bottomHeight}
+      class="h-20 flex justify-center items-center"
+    >
+      <div
+        on:click|stopPropagation
+        class="bg-primary py-4 px-5 space-x-4 rounded-xl flex"
+      >
+        {#each $images as _, index}
+          <div
+            class="
+              h-2 w-2 rounded-full
+              {index === imageIndex ? 'bg-white' : 'bg-gray'}
+            "
+          />
+        {/each}
       </div>
     </div>
   </div>
 {/if}
+<svelte:window on:keyup={handleKeyNavigation} />
 
 <style>
   .switch-button {
-    @apply w-15 h-15 flex justify-center items-center rounded-full bg-primary text-white;
+    @apply w-15 h-15 flex justify-center items-center rounded-full bg-primary text-white cursor-pointer hover:bg-gray;
   }
 </style>
